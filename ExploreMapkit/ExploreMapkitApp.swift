1d1ceff34448a0eec32eb25bfe58c0842c7150b0
rivaldofez <rivaldofez@gmail.com>
2022-08-10 11:15:44 +0700

    initialization project

---
+++ ExploreMapkit/ExploreMapkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreMapkitApp.swift
+//  ExploreMapkit
+//
+//  Created by Rivaldo Fernandes on 10/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ExploreMapkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
